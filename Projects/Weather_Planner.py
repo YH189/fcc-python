245e6c1d557fb456f4f79c6fdbeb98532bc55e09
YH <yhrj4874@gmail.com>
2026-08-14 13:58:51 +0530

    Implement weather-based transportation decision logic

---
+++ Projects/Weather_Planner.py
@@ -0,0 +1,23 @@
+distance_mi = 0
+is_raining = True
+has_bike = True
+has_car = True
+has_ride_share_app = False
+
+if(not distance_mi):
+    print(False)
+elif(distance_mi<=1):
+    if(is_raining != True):
+        print(True)
+    else:
+        print(False)
+elif(distance_mi <= 6):
+    if(has_bike and is_raining != True):
+        print(True)
+    else:
+        print(False)
+else:
+    if(has_car or has_ride_share_app):
+        print(True)
+    else:
+        print(False)
